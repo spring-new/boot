d5af7e73177d53c6f0a933110dea69c9c65d070d
1332854729@qq.com <1332854729@qq.com>
2022-08-01 20:05:17 +0800

    repository

---
+++ wa/src/main/java/com/hs/wa/WaApplication.java
@@ -0,0 +1,13 @@
+package com.hs.wa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaApplication.class, args);
+    }
+
+}
---
+++ wa/src/test/java/com/hs/wa/WaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.wa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
